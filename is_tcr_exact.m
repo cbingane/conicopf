e4abeb2a12f225ae939d0ecc588c067c7d55bb55
Christian Bingane <48734309+cbingane@users.noreply.github.com>
2019-08-15 20:34:49 -0400

    Add files via upload

--- is_tcr_exact.m
+++ is_tcr_exact.m
@@ -1,10 +1,16 @@
 % 'is_tcr_exact.m' measures the exactness of the tight-and-cheap relaxation
 % for the ACOPF problem
-function [optval, opttcr, optgap, solgap, error, isexact] = is_tcr_exact(casedata,model)
+% INPUTS
+%   casedata: MATPOWER case
+%   model: either 0 for loss minimization or 1 for cost minimization
+function [optval, opttcr, error, optgap, solgap, isexact] = is_tcr_exact(casedata,model)
 [Vtcr, vtcr, ~, ~, opttcr] = solve_opf_tcr(casedata,model);
 vtcr = vtcr(:,1);
-[optval, optsol] = solve_opf(casedata,model);
-optgap = 100*(1 - opttcr/max(opttcr,optval));
-solgap = 100*norm(optsol - vtcr)/norm(optsol);
+% Exactness error (epsilon)
 error = 100*max(1 -  abs(vtcr)./sqrt(diag(Vtcr)));
+[optval, optsol] = solve_opf(casedata,model);
+% Optimality gap (gamma)
+optgap = 100*(1 - opttcr/max(opttcr,optval));
+% Optimality distance (rho)
+solgap = 100*norm(optsol - vtcr)/norm(optsol);
 isexact = (error < 0.01);
