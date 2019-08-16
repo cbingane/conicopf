a45c30029d1be4216d55612153b765ca30455ead
Christian Bingane <48734309+cbingane@users.noreply.github.com>
2019-08-15 20:26:52 -0400

    Add files via upload

--- solve_acopf/solve_opf.m
+++ solve_acopf/solve_opf.m
@@ -1,5 +1,8 @@
 % 'solve_opf.m' solves the ACOPF problem and provides the optimal value and
 % the optimal bus voltages
+% INPUTS
+%   casedata: MATPOWER case
+%   model: either 0 for loss minimization or 1 for cost minimization
 function [optval,optsol] = solve_opf(casedata,model)
 casedata = loadcase(casedata);
 mpc = ext2int(casedata);
@@ -16,12 +19,10 @@ if model == 0
         mpc.gencost(:,6) = 0;
     end
     sol = runopf(mpc,mpopt);
-    optval = sol.f;
-    optsol = sol.x(size(mpc.bus,1)+1:2*size(mpc.bus,1)).*exp(1j*sol.x(1:size(mpc.bus,1)));
 else
     % COST MINIMIZATION
     sol = runopf(mpc,mpopt);
-    optval = sol.f;
-    optsol = sol.x(size(mpc.bus,1)+1:2*size(mpc.bus,1)).*exp(1j*sol.x(1:size(mpc.bus,1)));
 end
+optval = sol.f;
+optsol = sol.x(size(mpc.bus,1)+1:2*size(mpc.bus,1)).*exp(1j*sol.x(1:size(mpc.bus,1)));
 end
